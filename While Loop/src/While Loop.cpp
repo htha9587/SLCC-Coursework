d078fafff34268a29e6b6f9a552dfebe04f8c804
Harrison Thacker <htha9587@users.noreply.github.com>
2021-08-09 19:34:07 -0600

    Add files via upload

---
+++ While Loop/src/While Loop.cpp
@@ -0,0 +1,24 @@
+//============================================================================
+// Name        : While Loop.cpp
+// Author      : htha9587
+// Version     : 0.1.
+// Copyright   : Your copyright notice.
+// Description : Introduction to While loops.
+//============================================================================
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int i = 1;
+
+	while(i<=50)
+
+	{
+		i = i+1;
+		cout << i << "\n";
+		i++;
+	}
+	return 0;
+}
