15a168f97cb6071cb60b9000a67ee0816cc491ea
Harrison Thacker <htha9587@users.noreply.github.com>
2021-08-09 19:24:01 -0600

    Add files via upload

---
+++ Math/src/Math.cpp
@@ -0,0 +1,18 @@
+/*
+ * Math.cpp
+ *
+ *  Created on: Sep 1, 2016
+ *      Author: harri
+ */
+
+#include "Math.h"
+
+Math::Math() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Math::~Math() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Math/src/Math.h
@@ -0,0 +1,17 @@
+/*
+ * Math.h
+ *
+ *  Created on: Sep 1, 2016
+ *      Author: harri
+ */
+
+#ifndef MATH_H_
+#define MATH_H_
+
+class Math {
+public:
+	Math();
+	virtual ~Math();
+};
+
+#endif /* MATH_H_ */
